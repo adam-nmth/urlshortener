22042c16f1f957e9ad8dafba060d998b42b14dd6
Adam Nemeth <adamnemeth.contact@gmail.com>
2016-08-07 17:26:14 +0000

    initial commit

--- server.js
+++ server.js
@@ -1,9 +1,55 @@
 var express = require('express');
 var app = express();
+var validUrl = require('valid-url');
+var shortid = require('shortid');
+var mongo = require('mongodb').MongoClient
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var database = "urls"
+var url = "mongodb://localhost:27017/"+database;
+var db
+mongo.connect(url, (err, database) => {
+  if (err) return console.log(err)
+  db = database
+})
+
+app.use(express.static('public', { redirect : false }));
+
+app.get('/', function (req, res){
+    res.sendFile('index.html')
+})
+ 
+app.get('/:tag', function (req, res) {
+    var tag = req.params.tag
+    db.collection('urlcollection').find({"_id":tag}).toArray(function(err, results){
+        if(err) throw("no records found")
+        if(results.length!==0){
+        res.redirect(results[0]["realurl"])
+        }else{
+            res.send("no document found")
+        }
+    })
 });
+
+app.get('/new/:id*', function (req, res) {
+    var newurl = req.params['id'] + req.params[0];
+    if (validUrl.isUri(newurl)){
+        var rand = shortid.generate();
+        var upload = {
+            "_id": rand,
+            "realurl": newurl
+        }
+        var message = {
+            "original_url": newurl,
+            "short_url": "https://nodejsprojects-mr-nemeth.c9users.io/"+rand
+        }
+        db.collection('urlcollection').insert(upload)
+        res.send(message)
+        
+    } else {
+        res.send("{'error': 'wrong url format'}");
+    }
+});
+
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
